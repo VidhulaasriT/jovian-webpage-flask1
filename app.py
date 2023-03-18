cd7a4a6754528f2d29cdcf71b768f8498d16ca63
VidhulaasriT <127877220+VidhulaasriT@users.noreply.github.com>
2023-03-18 14:51:54 +0000

    commiting and going back to html&css - navbar not working

--- app.py
+++ app.py
@@ -1,43 +1,9 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
+
 app = Flask(__name__)
-
-
-JOBS=[
-  {
-    'id':1,
-    'title':'Data Analyst',
-    'location':'Bengaluru,India',
-    'salary':'Rs. 15,00,000'
-  },
-  {
-    'id':2,
-    'title':'Data Scientist',
-    'location':'Delhi,India',
-    'salary':'Rs. 12,00,000'
-  },
-  {
-    'id':3,
-    'title':'Frontend Engineer',
-    'location':'Remote',
-    'salary':'Rs. 13,00,000'
-  },
-  {
-    'id':4,
-    'title':'Backend Engineer',
-    'location':'San Fransisco,USA',
-    'salary':'$18,00,000'
-  }
-]
-
-
-
 @app.route("/")
-def hello_world():
-  return render_template("home.html",jobs=JOBS,company_name="Jovian")
-
-@app.route("/api/jobs")
-def list_jobs():
-  return jsonify(JOBS)
+def hellow_world():
+  return render_template("home.html")
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0",debug=True)
